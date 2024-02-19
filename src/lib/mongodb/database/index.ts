2bc9926cc19a00d281adf0a3a220ecd17966d13a
Yogesh Kumar <yksainimbd@gmail.com>
2024-02-20 05:25:16 +0530

    bug fixed: rename a file

--- src/lib/mongodb/database/index.ts
+++ src/lib/mongodb/database/index.ts
@@ -1,8 +1,8 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, Promise: null};
+let cached = (global as any).mongoose || { conn: null, promise: null};
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
