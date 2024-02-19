f2324913632e7516f3c80e8127743e967b33ceb4
Yogesh Kumar <yksainimbd@gmail.com>
2024-02-19 22:05:22 +0530

    footer done

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className='border-t'>
+        <div className='flex-center wrapper flex-between flex-container flex-col gap-4 p-5 text-center sm:flex-row'>
+          <Link href='/'>
+            <Image
+              alt='logo'
+              src='/assets/images/logo.svg'
+              width={138}
+              height={38}
+            />
+          </Link>
+          <p>2024 Events. All right reserved!</p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
